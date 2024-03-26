18b1eb28709626a394b444a294aeda43ac6ad064
sariditri31@gmail.com <sariditri31@gmail.com>
2024-03-25 20:54:46 -0500

    reload and try ejecution

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,5 +1,6 @@
 import { diaryEntry } from "../types";
 import diaryData from "./diaries";
+// typescriptprimero busca las extensiones de esta manera ".tsx", ".ts", ".node" ".js" ".json";
 
 const diaries: Array<diaryEntry> = diaryData as Array<diaryEntry>; // Asercion de tipos recordar y repasar
 
